2c6cb3dba7fcbcc904eb7310ffbdd536df740abf
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2020-03-25 11:48:09 -0500

    updated client origin

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recruitr',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recruitr_test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recruitr_test',
+    CLIENT_ORIGIN: `http://localhost:3000`
 }
