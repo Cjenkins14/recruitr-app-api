c67c284a47d957486c3e8799ff26620c5d039175
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2020-03-28 09:48:10 -0500

    changed client  origin for prod deploy

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recruitr',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recruitr_test',
-    CLIENT_ORIGIN: `http://localhost:3000`
+    CLIENT_ORIGIN: `https://recruitr-app-d14s0am8a.now.sh/`
 }
